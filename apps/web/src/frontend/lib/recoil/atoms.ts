495f26d7b1c11bde0511fed653c230cfcb7bcaf0
Hayoung <gkduddy0814@gmail.com>
2023-04-25 00:19:29 +0900

    fix

--- apps/web/src/frontend/lib/recoil/atoms.ts
+++ apps/web/src/frontend/lib/recoil/atoms.ts
@@ -1,5 +1,4 @@
 import { ModalProps } from '@/types/modal';
-import { EntireUserTest } from '@/types/test';
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 import { v1 } from 'uuid';
@@ -9,17 +8,6 @@ export const TestRecoilKey = `/test/extraction/${v1()}`;
 export const ModalRecordkey = `createModal/${v1()}`;
 
 export const { persistAtom } = recoilPersist();
-
-export const subjectState = atom<Subject | null>({
-  key: SubjectRecoilKey,
-  default: null,
-  // effects_UNSTABLE: [persistAtom],
-});
-
-export const testState = atom<EntireUserTest>({
-  key: TestRecoilKey,
-  default: {} as EntireUserTest,
-});
 
 export const modalState = atom<ModalProps>({
   key: ModalRecordkey,
