f349de39b3f16a7d45c187bf8a6f0e48670552ac
Hayoung <gkduddy0814@gmail.com>
2023-04-26 10:16:26 +0900

    fix

--- apps/web/src/types/error.ts
+++ apps/web/src/types/error.ts
@@ -1,5 +1,17 @@
-import { ERROR_MAP } from 'mainlib/constants/error';
-
+export const ERROR_MAP = {
+  DUP01: {
+    code: 'DUP01',
+    message: '이미 중복된 항목이 존재해요.',
+  },
+  DUP02: {
+    code: 'DUP02',
+    message: '이미 처리가 되었어요.',
+  },
+  FOUND01: {
+    code: 'FOUND01',
+    message: '조회 결과가 존재하지 않아요.',
+  },
+} as const;
 export type ErrorKeys = keyof typeof ERROR_MAP;
 export type ErrorCodeKeys = keyof (typeof ERROR_MAP)[ErrorKeys];
 export type ErrorCodes = (typeof ERROR_MAP)[ErrorKeys]['code'] | undefined;
